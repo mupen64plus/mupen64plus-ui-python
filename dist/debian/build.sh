9f88e4fd2973d03c17aaa43c760959d6fdcf2b57
Fayvel Victor <fayvel@web.de>
2014-09-04 10:03:11 +0200

    fix build dependencies of debian package

--- dist/debian/build.sh
+++ dist/debian/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-apt-get -y install build-essential debhelper pyqt4-dev-tools
+apt-get -y install build-essential debhelper pyqt4-dev-tools dh-python python python-qt4 pyqt4-dev-tools
 cp -fr ../debian/ ../../
 cd ../../
 dpkg-buildpackage -rfakeroot
