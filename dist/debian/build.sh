24ed63a0116401815f4cb6617a472517f0010029
Fayvel Victor <fayvel@web.de>
2014-09-04 10:17:51 +0200

    cleanup debian build path before trying to build again

--- dist/debian/build.sh
+++ dist/debian/build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 apt-get -y install build-essential debhelper pyqt4-dev-tools dh-python python python-qt4 pyqt4-dev-tools
+rm -rf ../../debian/
 cp -fr ../debian/ ../../
 cd ../../
 dpkg-buildpackage -rfakeroot
