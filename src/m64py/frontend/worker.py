24638200e36a94a77ff969f24764a6b46ebb7053
Milan Nikolic <gen2brain@gmail.com>
2020-04-06 13:19:35 +0200

    Fix screenshot

--- src/m64py/frontend/worker.py
+++ src/m64py/frontend/worker.py
@@ -206,8 +206,8 @@ class Worker(QThread):
 
     def get_screenshot(self, path):
         """Gets last saved screenshot."""
-        rom_name = str(self.core.rom_header.Name).replace(
-            ' ', '_').lower()
+        name = self.core.rom_header.Name.decode()
+        rom_name = name.replace(' ', '_').lower()
         screenshots = []
         for filename in os.listdir(path):
             if filename.startswith(rom_name):
